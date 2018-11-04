528910c49e4564820b15dd762b2cfa6683a6dc52
S Fujimoto <unicoshun@gmail.com>
2018-11-04 16:56:12 +0900

    settings to settings file

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 import pygame
 import random
-
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-DARKGREY = (27, 140, 141)
-LIGHTGREY = (189, 195, 199)
-GREEN = (60, 186, 84)
-RED = (219, 50, 54)
-YELLOW = (244, 194, 13)
-BLUE = (72, 133, 237)
+from settings import *
 
 # initialize pygame and create window
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((HEIGHT, WIDTH))
-pygame.display.set_caption("My Game")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
---
+++ settings.py
@@ -0,0 +1,15 @@
+# game options/settings
+TITLE = "My Game"
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (27, 140, 141)
+LIGHTGREY = (189, 195, 199)
+GREEN = (60, 186, 84)
+RED = (219, 50, 54)
+YELLOW = (244, 194, 13)
+BLUE = (72, 133, 237)
