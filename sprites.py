a2253de49fb36e8ba5761eb706fbaa455f0907a0
S Fujimoto <unicoshun@gmail.com>
2018-11-09 17:44:56 +0900

    add list frames

--- sprites.py
+++ sprites.py
@@ -10,13 +10,36 @@ class Player(pg.sprite.Sprite):
     def __init__(self, game):
         super().__init__()
         self.game = game
-        self.image = self.game.spritesheet.get_image(614, 1063, 120, 191)
+        self.walking = False
+        self.jumping = False
+        self.current_frame = 0  # to keep track of animation frame
+        self.last_update = 0  # to keep time of animation
+        self.load_images()
+        self.image = self.standing_frames[0]
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         self.pos = vec(WIDTH / 2, HEIGHT / 2)
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+        self.standing_frames = []
+        self.walk_frames_r = []
+        self.walk_frames_l = []
+        self.jump_frame = []
+
+    def load_images(self):
+        self.standing_frames = [
+            self.game.spritesheet.get_image(614, 1063, 120, 191),
+            self.game.spritesheet.get_image(584, 0, 121, 201),
+        ]
+        self.walk_frames_r = [
+            self.game.spritesheet.get_image(678, 860, 120, 201),
+            self.game.spritesheet.get_image(692, 1458, 120, 207),
+        ]
+        self.walk_frames_l = []
+        for frame in self.walk_frames_r:
+            self.walk_frames_l.append(pg.transform.flip(frame, True, False))
+        self.jump_frame = self.game.spritesheet.get_image(382, 763, 150, 181)
 
     def jump(self):
         # jump only if on a platform
